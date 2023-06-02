482a3e8d03351668272010a74b288ca379fbb107
Sandesh Patel <sandesh.patel@nutanix.com>
2023-06-02 12:50:36 +0000

    edt time stamp traffic shaper

--- network/bpf/traffic_shaping/net_shaper.c
+++ network/bpf/traffic_shaping/net_shaper.c
@@ -12,16 +12,16 @@
 
 #define SLEEP_TIME 1
 #define NCLASSES 2
-#define MB 1024*1024
+#define MB 1024 * 1024
 
-const char* rate_map = "/sys/fs/bpf/tc/globals/rate_map";
-const char* egress_stats_map = "/sys/fs/bpf/tc/globals/egress_stats";
+const char *rate_map = "/sys/fs/bpf/tc/globals/rate_map";
+const char *egress_stats_map = "/sys/fs/bpf/tc/globals/egress_stats";
 
-uint64_t reservation[] = {450*1024*1024, 700*1024*1024};
-uint64_t BWE = 1175*1024*1024;
+uint64_t reservation[] = {915 * 1024 * 1024, 200 * 1024 * 1024};
+uint64_t BWE = 1175 * 1024 * 1024;
 
-
-int map_open(const char* filepath){
+int map_open(const char *filepath)
+{
     int fd = bpf_obj_get(filepath);
     if (fd < 0)
     {
@@ -35,13 +35,16 @@ int map_open(const char* filepath){
     return fd;
 }
 
-static inline int calculate_expected_bw(uint64_t rate, uint64_t reservation){
+static inline int calculate_expected_bw(uint64_t rate, uint64_t reservation)
+{
     // ensure non-zero expected bandwidth
     // return 2*rate < reservation ? 2*rate+10 * MB : reservation;
-    return rate < reservation ? BWE : reservation;
+    // return rate < reservation ? BWE : reservation;
+    return reservation;
 }
 
-int shaper(){
+int shaper()
+{
     uint64_t *temp, *current, *last;
     uint64_t unused = 0, used = 0;
     uint64_t bytes_sent_latest[NCLASSES] = {0};
@@ -51,7 +54,8 @@ int shaper(){
     last = &(bytes_sent_last[0]);
     int rate_fd = map_open(rate_map);
     int stats_fd = map_open(egress_stats_map);
-    for(int i=0; i<NCLASSES; i++){
+    for (int i = 0; i < NCLASSES; i++)
+    {
         /*
         make sure the array sizes match
         */
@@ -59,8 +63,10 @@ int shaper(){
     }
     // read current stats
     uint64_t duration = 1;
-    while(1){
-        for(int i=0; i<NCLASSES; i++){
+    while (1)
+    {
+        for (int i = 0; i < NCLASSES; i++)
+        {
             /*
                 try other signature to check for errors
             */
@@ -68,34 +74,44 @@ int shaper(){
         }
 
         unused = 0;
-        used = 0 ;
-        for(int i=0; i<NCLASSES; i++){
+        used = 0;
+        for (int i = 0; i < NCLASSES; i++)
+        {
             uint64_t bw_i = (current[i] - last[i]) / SLEEP_TIME;
-            
-            bw_rate[i] = calculate_expected_bw(bw_i, reservation[i]);
-            printf("class %d\tbw mb: %lu\t\t bytes %lu \t\t\t next-rate mb %lu\n", i, bw_i/(MB), current[i], bw_rate[i]/MB);
-            if (bw_i < reservation[i]){
-                unused += (reservation[i] - bw_i)/2;
-            } else {
-                used += reservation[i];
-            }
+
+            // bw_rate[i] = calculate_expected_bw(bw_i, reservation[i]);
+            printf("class %d\tbw mb: %lu\t\t bytes %lu \t\t\t next-rate mb %lu\n", i, bw_i / (MB), current[i], bw_rate[i] / MB);
+            // if (bw_i < reservation[i]){
+            //     unused += (reservation[i] - bw_i);
+            // } else {
+            //     used += reservation[i];
+            // }
+            // unused += (reservation[i] - bw_i);
         }
+        printf("unused bw %lu", unused / (MB));
+        printf("\n\n");
         temp = last;
         last = current;
         current = temp;
 
-        for(int i=0; i<NCLASSES; i++){
-            if (bw_rate[i] == reservation [i]){
-                bw_rate[i] += (reservation[i]*unused) / (used);
-            }
-            bpf_map_update_elem(rate_fd, &i, &bw_rate[i], BPF_ANY);
-        }
+        // for(int i=0; i<NCLASSES; i++){
+        //     if (bw_rate[i] == reservation [i]){
+        //         bw_rate[i] += (reservation[i]*unused) / (used);
+        //     }
+        //     bpf_map_update_elem(rate_fd, &i, &bw_rate[i], BPF_ANY);
+        // }
+        // for(int i=NCLASSES; i<NCLASSES+1; i++){
+        //     // if (bw_rate[i] == reservation [i]){
+        //     //     bw_rate[i] += (reservation[i]*unused) / (used);
+        //     // }
+        //     bpf_map_update_elem(rate_fd, &i, &unused, BPF_ANY);
+        // }
         sleep(SLEEP_TIME);
     }
     return 0;
 }
 
-
-int main(){
+int main()
+{
     return shaper();
 }
--- network/bpf/traffic_shaping/rate_filter.c
+++ network/bpf/traffic_shaping/rate_filter.c
@@ -8,36 +8,42 @@
 #include <arpa/inet.h>
 #include <linux/sched.h>
 
-
 /* the maximum delay we are willing to add (drop packets beyond that) */
 #define TIME_HORIZON_NS (2000 * 1000 * 1000)
+#define TIME_HORIZON_BW_NS (100 * 1000 * 1000)
 #define NS_PER_SEC 1000000000
 #define ECN_HORIZON_NS 5000000
 #define THROTTLE_RATE_BPS (90 * 1000 * 1000)
+#define MB (1024 * 1024)
 
+#define R1 (900 * MB)
+#define R2 (210 * MB)
 
 /* flow_key => last_tstamp timestamp used */
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 100);
-    __type(key, __u32);
-    __type(value, __u64);
+struct
+{
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 100);
+	__type(key, __u32);
+	__type(value, __u64);
 } ts_map SEC(".maps");
 
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 100);
-    __type(key, __u32);
-    __type(value, __u64);
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
+struct
+{
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 100);
+	__type(key, __u32);
+	__type(value, __u64);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
 } rate_map SEC(".maps");
 
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 100);
-    __type(key, __u32);
-    __type(value, __u64);
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
+struct
+{
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__uint(max_entries, 100);
+	__type(key, __u32);
+	__type(value, __u64);
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
 } egress_stats SEC(".maps");
 
 // static inline int classify(struct __sk_buff *skb){
@@ -49,54 +55,111 @@ struct {
 // 	return 0;
 // }
 
-static inline int classify(struct __sk_buff *skb){
-	const int l3_off = ETH_HLEN;                       // IP header offset
-    const int l4_off = l3_off + sizeof(struct iphdr);  // TCP header offset
-    const int l7_off = l4_off + sizeof(struct tcphdr); // L7 (e.g. HTTP) header offset
-    void *data = (void*)(long)skb->data;
-    void *data_end = (void*)(long)skb->data_end;
+static inline int classify(struct __sk_buff *skb)
+{
+	const int l3_off = ETH_HLEN;					   // IP header offset
+	const int l4_off = l3_off + sizeof(struct iphdr);  // TCP header offset
+	const int l7_off = l4_off + sizeof(struct tcphdr); // L7 (e.g. HTTP) header offset
+	void *data = (void *)(long)skb->data;
+	void *data_end = (void *)(long)skb->data_end;
 
-    if (data_end < data + l7_off)
-        return 0; // Not our packet, handover to kernel
+	if (data_end < data + l7_off)
+		return 0; // Not our packet, handover to kernel
 
-    struct ethhdr *eth = data;
-    if (eth->h_proto != htons(ETH_P_IP))
-       return 0; // Not an IPv4 packet, handover to kernel
+	struct ethhdr *eth = data;
+	if (eth->h_proto != htons(ETH_P_IP))
+		return 0; // Not an IPv4 packet, handover to kernel
 
-    struct iphdr *ip = (struct iphdr *)(data + l3_off);
-    if (ip->protocol != IPPROTO_TCP)
-        return 0;
+	struct iphdr *ip = (struct iphdr *)(data + l3_off);
+	if (ip->protocol != IPPROTO_TCP)
+		return 0;
 
-    // struct tcphdr *tcp = (struct tcphdr *)(data + l4_off);
-    __u32 key = ip->saddr;
-    __u32 target_ip = htonl(0x0A600455);
-	if (key == target_ip){
+	// struct tcphdr *tcp = (struct tcphdr *)(data + l4_off);
+	__u32 key = ip->saddr;
+	__u32 target_ip = htonl(0x0A600455);
+	if (key == target_ip)
+	{
 		return 1;
 	}
 
-    return 0;
+	return 0;
 }
 
 static inline int throttle_flow(struct __sk_buff *skb)
 {
-	uint32_t key = classify(skb);
+	uint64_t t1, t2, *ts1, *ts2;
+	uint64_t *r1, *r2;
+	uint64_t bw, borrow = 0;
+	uint32_t key1 = classify(skb);
+	uint32_t key = key1;
+	uint32_t key2 = 1 - key;
+	uint64_t rates[2] = {R1, R2};
+	// todo: read reservation from maps
+	//  r1 = bpf_map_lookup_elem(&rate_map, &key);
+
+	r1 = &rates[key1];
+	r2 = &rates[key2];
+
 	/*
-	set default throttle rate
+	todo: ensure sanity of read timestamps
 	*/
-	uint64_t default_throttle_rate = THROTTLE_RATE_BPS;
-	uint64_t *throttle_rate = bpf_map_lookup_elem(&rate_map, &key);
-	if (!throttle_rate || !(*throttle_rate)){
-		throttle_rate = &default_throttle_rate;
-	}
-	uint64_t *last_tstamp = bpf_map_lookup_elem(&ts_map, &key);
-
-	uint64_t *bytes_sent = bpf_map_lookup_elem(&egress_stats, &key);
-	if (bytes_sent){
-		__sync_fetch_and_add(bytes_sent, skb->len);
-	}
-
-	uint64_t delay_ns = (((uint64_t)skb->len) * NS_PER_SEC) / ((*throttle_rate+1));
+	ts1 = bpf_map_lookup_elem(&ts_map, &key1);
+	ts2 = bpf_map_lookup_elem(&ts_map, &key2);
 	uint64_t now = bpf_ktime_get_ns();
+	if (!ts1 || *ts1 < now)
+	{
+		t1 = now;
+	}
+	else
+	{
+		t1 = *ts1;
+	}
+	if (!ts2 || *ts2 < now)
+	{
+		t2 = now;
+	}
+	else
+	{
+		t2 = *ts2;
+	}
+	if (t1 < t2)
+	{
+		if (t2 - t1 >= TIME_HORIZON_BW_NS)
+		{
+			bw = *r1 / 1000;
+		}
+		else
+		{
+			// bw = *r1 - (((t2 - t1)* (*r1))/TIME_HORIZON_BW_NS);// + *r1/1000;
+			uint32_t shift = ((t2 - t1) * 10) / TIME_HORIZON_BW_NS;
+			uint64_t factor = 1;
+			factor = factor << shift;
+			bw = *r1 / factor;
+			// bw = *r1;
+		}
+	}
+	else
+	{
+		bw = *r1;
+		// calculate borrow percentage
+		borrow = ((t1 - t2) * 100) / TIME_HORIZON_BW_NS;
+		if (borrow > 99)
+		{
+			borrow = 99;
+		}
+		bw += (borrow * (*r2)) / 100;
+	}
+
+	// uint64_t default_throttle_rate = THROTTLE_RATE_BPS;
+	// uint64_t *throttle_rate = bpf_map_lookup_elem(&rate_map, &key);
+	// if (!throttle_rate || !(*throttle_rate)){
+	// 	throttle_rate = &default_throttle_rate;
+	// }
+	uint64_t *throttle_rate = &bw;
+	// uint64_t *last_tstamp = bpf_map_lookup_elem(&ts_map, &key);
+	uint64_t *last_tstamp = &t1;
+	uint64_t delay_ns = (((uint64_t)skb->len) * NS_PER_SEC) / ((*throttle_rate + 1));
+	// uint64_t now = bpf_ktime_get_ns();
 	uint64_t tstamp, next_tstamp = 0;
 
 	if (last_tstamp)
@@ -107,8 +170,10 @@ static inline int throttle_flow(struct __sk_buff *skb)
 		tstamp = now;
 
 	/* should we throttle? */
-	if (next_tstamp <= tstamp) {
-		if (bpf_map_update_elem(&ts_map, &key, &tstamp, BPF_ANY))
+	if (next_tstamp <= tstamp)
+	{
+		// __sync_fetch_and_add(last_tstamp, delay_ns);
+		if (bpf_map_update_elem(&ts_map, &key, &next_tstamp, BPF_ANY))
 			return TC_ACT_SHOT;
 		return TC_ACT_OK;
 	}
@@ -121,16 +186,23 @@ static inline int throttle_flow(struct __sk_buff *skb)
 	if (next_tstamp - now >= ECN_HORIZON_NS)
 		bpf_skb_ecn_set_ce(skb);
 
+	// update time stamp
+	// __sync_fetch_and_add(last_tstamp, delay_ns);
 	if (bpf_map_update_elem(&ts_map, &key, &next_tstamp, BPF_EXIST))
 		return TC_ACT_SHOT;
 	skb->tstamp = next_tstamp;
 
+	uint64_t *bytes_sent = bpf_map_lookup_elem(&egress_stats, &key);
+	if (bytes_sent)
+	{
+		__sync_fetch_and_add(bytes_sent, skb->len);
+	}
+
 	return TC_ACT_OK;
 }
 
-__attribute__((section("cls_test"), used))
-int tc_prog(struct __sk_buff *skb)
-{	
+__attribute__((section("cls_test"), used)) int tc_prog(struct __sk_buff *skb)
+{
 	return throttle_flow(skb);
 }
 char __license[] SEC("license") = "GPL";
